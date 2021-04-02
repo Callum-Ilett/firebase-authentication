84d0da5eb790292501207ca0384492b45fa769c3
Callum Ilett <cilett99@gmail.com>
2021-04-02 23:12:50 +0100

    Completed Authentication

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_FIREBASE_AUTH_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-// };
-
 const firebaseConfig = {
   apiKey: "AIzaSyC-KNy3ayOTwYi79g0QVqFOBA192CSxVB0",
   authDomain: "fir-demo-d2f53.firebaseapp.com",
